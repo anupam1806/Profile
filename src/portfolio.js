af519f9683c28fb48db34901637eb9590ab0ed8c
Anupam <anupam.saini18@gmail.com>
2021-10-01 22:25:32 +0530

    project update

--- src/portfolio.js
+++ src/portfolio.js
@@ -211,7 +211,7 @@ export const projects = [
 	},
 	{
 		name: "Google keep",
-		desc: "It contains basic banking operations such as transfer",
+		desc: "as transfer",
 		link: "https://keep-google.herokuapp.com/",
 		github: "https://github.com/anupam1806/keeper",
 	},
