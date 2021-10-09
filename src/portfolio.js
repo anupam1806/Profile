d28cb12128294c40d265ae64237eaac3e0f82e70
Anupam <anupam.saini18@gmail.com>
2021-10-09 21:37:52 +0530

    project update

--- src/portfolio.js
+++ src/portfolio.js
@@ -244,5 +244,5 @@ export const feedbacks = [
 	// {
 	// 	name: "Maheen Altaf",
 	// 	feedback:
-	// 		"the website is very nice, everything was perfectly made as i need. it just loads
+	// 		"the website is very nice, everything was perfectly made as i need.
 ];
